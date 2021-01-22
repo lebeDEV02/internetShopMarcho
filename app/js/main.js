c5e709c618a065798abef4416146bf8c1ac2d76d
ledeDEV02 <vladimirlebedev00010@gmail.com>
2021-01-22 15:54:31 +0500

    added promo and partners

--- app/js/main.js
+++ app/js/main.js
@@ -13,4 +13,46 @@ $(function () {
 		ratedFill: "#ffc35b",
 		readOnly: true
 	});
+	function getTimeRemaining(endtime) {
+		const total = Date.parse(endtime) - Date.parse(new Date());
+		const seconds = Math.floor((total / 1000) % 60);
+		const minutes = Math.floor((total / 1000 / 60) % 60);
+		const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+		const days = Math.floor(total / (1000 * 60 * 60 * 24));
+
+		return {
+			total,
+			days,
+			hours,
+			minutes,
+			seconds
+		};
+	}
+
+	function initializeClock(clockClass, endtime) {
+		const clock = document.querySelector(clockClass);
+		const daysSpan = clock.querySelector('.promo__days');
+		const hoursSpan = clock.querySelector('.promo__hours');
+		const minutesSpan = clock.querySelector('.promo__minutes');
+		const secondsSpan = clock.querySelector('.promo__seconds');
+
+		function updateClock() {
+			const t = getTimeRemaining(endtime);
+
+			daysSpan.innerHTML = t.days;
+			hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+			minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+			secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+			if (t.total <= 0) {
+				clearInterval(timeinterval);
+			}
+		}
+
+		updateClock();
+		const timeinterval = setInterval(updateClock, 1000);
+	}
+
+	const deadline = $('.promo__timer').attr('data-time');
+	initializeClock('.promo__timer', deadline);
 });
